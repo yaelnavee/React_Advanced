a469174b77129002a5a4dc7a31d18dc665426438
shiffy01 <sscheine@g.jct.ac.il>
2025-05-20 17:23:01 +0300

    push

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 
 function Login() {
-    const [us, setUs]=useState("nothing")
-    function func(){
-        const u=fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const [us, setUs] = useState("nothing");
+
+  function func() {
+    fetch('http://localhost:3000/users/1')
       .then(response => response.json())
-      .then(json => console.log(json))
-setUs(u)
-    }
+      .then(json => {
+        console.log(json);
+        setUs(JSON.stringify(json)); // convert object to string for display
+      })
+      .catch(err => {
+        console.error(err);
+        setUs("Error fetching data");
+      });
+  }
+
   return (
     <>
-    <>login will be here</>
-    <button onClick={func}>
-get
-    </button>
-     <div>{us}</div></>
-   
+      <>login will be here</>
+      <button onClick={func}>get</button>
+      <div>{us}</div>
+    </>
   );
 }
+
 
 
 export default Login;
